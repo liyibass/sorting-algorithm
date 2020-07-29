185f24382f7862039f55b00fd7ec5da04dfe16e2
Liyi <xmage211120@gmail.com>
2020-07-30 01:40:01 +0800

    Add prompts to introduce each algorithm,and add simple RWD for prompt section.

--- src/components/SortingVisualizer/SortingVisualizer.component.jsx
+++ src/components/SortingVisualizer/SortingVisualizer.component.jsx
@@ -11,6 +11,7 @@ import {
 function SortingVisualizer() {
   const [Diagram, setDiagram] = useState([]); //拿來顯示的Diagram
   const [resultDiagram, setResultDiagram] = useState([]); //背景顯示結果的Diagram
+  const [prompt, setPrompt] = useState({});
   const EXECUDE_TIME_INTERVAL = 6; //步驟執行時間
   const [timeouts, setTimeouts] = useState([]); // 儲存動畫的timeouts id，讓我們可以在reset時可以停止動畫
 
@@ -30,11 +31,14 @@ function SortingVisualizer() {
 
     // 計算螢幕寬度以及bar數量，依此建立DiagramArray
     const screenWidth = document.body.clientWidth;
-    const barNumber = Math.floor(screenWidth / 8);
+    const barNumber = Math.floor(screenWidth / 10);
     let DiagramArray = [];
     for (let i = 0; i < barNumber; i++) {
       DiagramArray[i] = Math.floor(Math.random() * 100);
     }
+    const copyDiagram = DiagramArray.slice();
+    const result = insertionSortAnimations(copyDiagram);
+    setResultDiagram(result.Array);
 
     // 更新顯示畫面
     setDiagram(DiagramArray);
@@ -42,8 +46,11 @@ function SortingVisualizer() {
   };
 
   const clearDiagram = () => {
+    const showPrompt = document.querySelector(".promptContainer");
+    showPrompt.classList.remove("promptContainer-showPrompt");
     setDiagram([]);
     setResultDiagram([]);
+    setPrompt({});
   };
 
   // ----------------------------Algorithm Handler----------------------------
@@ -54,6 +61,12 @@ function SortingVisualizer() {
     // 顯示背景成果畫面、開啟動畫
     setResultDiagram(result.Array);
     animationHandler(result.animations);
+    setPrompt({
+      algorithm: "Selection Sort",
+      Best: `Best:O(n${String.fromCharCode(178)})`,
+      Worst: `Worst:O(n${String.fromCharCode(178)})`,
+      Avg: `Avg:O(n${String.fromCharCode(178)})`,
+    });
   };
 
   const bubbleSortHandler = () => {
@@ -61,6 +74,12 @@ function SortingVisualizer() {
     const result = bubbleSortAnimations(copyDiagram);
     setResultDiagram(result.Array);
     animationHandler(result.animations);
+    setPrompt({
+      algorithm: "Bubble Sort",
+      Best: `Best:O(n)`,
+      Worst: `Worst:O(n${String.fromCharCode(178)})`,
+      Avg: `Avg:O(n${String.fromCharCode(178)})`,
+    });
   };
 
   const insertionSortHandler = () => {
@@ -68,6 +87,12 @@ function SortingVisualizer() {
     const result = insertionSortAnimations(copyDiagram);
     setResultDiagram(result.Array);
     animationHandler(result.animations);
+    setPrompt({
+      algorithm: "Insertion Sort",
+      Best: `Best:O(n)`,
+      Worst: `Worst:O(n${String.fromCharCode(178)})`,
+      Avg: `Avg:O(n${String.fromCharCode(178)})`,
+    });
   };
 
   // ----------------------------Animation Handler----------------------------
@@ -93,6 +118,11 @@ function SortingVisualizer() {
             barTwoStyle.height,
             barOneStyle.height,
           ];
+
+          if (i === Math.floor(animations.length / 2)) {
+            const showPrompt = document.querySelector(".promptContainer");
+            showPrompt.classList.add("promptContainer-showPrompt");
+          }
         }, i * EXECUDE_TIME_INTERVAL)
       );
       timeouts.push(
@@ -104,7 +134,7 @@ function SortingVisualizer() {
       );
     }
   }
-
+  console.log("render");
   return (
     <div
       className="SortingVisualizer"
@@ -127,6 +157,13 @@ function SortingVisualizer() {
         {resultDiagram.map((bar, index) => {
           return <DiagramBar key={index} value={bar} />;
         })}
+      </div>
+
+      <div className="promptContainer ">
+        <h1>{prompt.algorithm}</h1>
+        <h2>{prompt.Best}</h2>
+        <h2>{prompt.Worst}</h2>
+        <h2>{prompt.Avg}</h2>
       </div>
     </div>
   );
