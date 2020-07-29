5f6fda479f28c8f606a5b5245dc442a8ef2616dd
Liyi <xmage211120@gmail.com>
2020-07-29 22:29:16 +0800

    Add clear and Generate button to prevent reset bug.

--- src/components/SortingVisualizer/SortingVisualizer.component.jsx
+++ src/components/SortingVisualizer/SortingVisualizer.component.jsx
@@ -11,51 +11,46 @@ import {
 function SortingVisualizer() {
   const [Diagram, setDiagram] = useState([]);
   const [resultDiagram, setResultDiagram] = useState([]);
-  const EXECUDE_TIME_INTERVAL = 10;
+  const EXECUDE_TIME_INTERVAL = 7;
+
   useEffect(() => {
-    resetDiagram();
+    generateDiagram();
   }, []);
 
-  const resetDiagram = () => {
-    const screenWidth = document.body.clientWidth;
-    const barNumber = Math.floor(screenWidth / 8);
-
-    let DiagramArray = [];
-    for (let i = 0; i < barNumber; i++) {
-      DiagramArray[i] = Math.floor(Math.random() * 100);
-    }
-
-    setDiagram(DiagramArray);
-    setResultDiagram([]);
-  };
-
-  const animationHandler = (animations) => {
+  // 儲存動畫的timeouts 在reset時可以停止動畫
+  const [timeouts, setTimeouts] = useState([]);
+  function animationHandler(animations) {
     // 取得所有bar
     const allArrayBars = document.querySelectorAll(".DiagramBar");
     // 依序將animation pair上色並交換高度
+
     for (let i = 0; i < animations.length; i++) {
       const [barOneIdx, barTwoIdx] = animations[i];
       const barOneStyle = allArrayBars[barOneIdx].style;
       const barTwoStyle = allArrayBars[barTwoIdx].style;
 
-      setTimeout(() => {
-        // 上色
-        barOneStyle.backgroundColor = "red";
-        barTwoStyle.backgroundColor = "red";
-        // 交換高度
-        [barOneStyle.height, barTwoStyle.height] = [
-          barTwoStyle.height,
-          barOneStyle.height,
-        ];
-      }, i * EXECUDE_TIME_INTERVAL);
-
-      setTimeout(() => {
-        // 上色過後立即還原顏色
-        barOneStyle.backgroundColor = "#4d88e0";
-        barTwoStyle.backgroundColor = "#4d88e0";
-      }, (i + 1) * EXECUDE_TIME_INTERVAL);
+      // 啟動setTimeout的同時將id放至timeouts中
+      timeouts.push(
+        window.setTimeout(() => {
+          // 上色
+          barOneStyle.backgroundColor = "red";
+          barTwoStyle.backgroundColor = "red";
+          // 交換高度
+          [barOneStyle.height, barTwoStyle.height] = [
+            barTwoStyle.height,
+            barOneStyle.height,
+          ];
+        }, i * EXECUDE_TIME_INTERVAL)
+      );
+      timeouts.push(
+        window.setTimeout(() => {
+          // 上色過後立即還原顏色
+          barOneStyle.backgroundColor = "#4d88e0";
+          barTwoStyle.backgroundColor = "#4d88e0";
+        }, (i + 1) * EXECUDE_TIME_INTERVAL)
+      );
     }
-  };
+  }
 
   const selectionSortHandler = () => {
     const copyDiagram = Diagram.slice();
@@ -78,14 +73,39 @@ function SortingVisualizer() {
     animationHandler(result.animations);
   };
 
-  console.log(window.innerHeight);
+  const generateDiagram = () => {
+    for (var i = 0; i < timeouts.length; i++) {
+      clearTimeout(timeouts[i]);
+    }
+    //quick reset of the timer array you just cleared
+    setTimeouts([]);
+
+    const screenWidth = document.body.clientWidth;
+    const barNumber = Math.floor(screenWidth / 8);
+
+    let DiagramArray = [];
+    for (let i = 0; i < barNumber; i++) {
+      DiagramArray[i] = Math.floor(Math.random() * 100);
+    }
+
+    setDiagram(DiagramArray);
+    setResultDiagram([]);
+  };
+
+  const clearDiagram = () => {
+    setDiagram([]);
+    setResultDiagram([]);
+  };
+  console.log("rerender");
+  console.log("");
   return (
     <div
       className="SortingVisualizer"
       style={{ height: `${window.innerHeight}px` }}
     >
       <div className="controlBar">
-        <button onClick={() => resetDiagram()}>Reset</button>
+        <button onClick={() => clearDiagram()}>Clear</button>
+        <button onClick={() => generateDiagram()}>Generate</button>
         <button onClick={() => selectionSortHandler()}>Selection Sort</button>
         <button onClick={() => bubbleSortHandler()}>Bubble Sort</button>
         <button onClick={() => insertionSortHandler()}>insertion Sort</button>
